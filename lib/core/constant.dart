7f85e6a9b0490c866880a426ecdb814f5905576a
dhasarathk7 <dhasarathk7@gmail.com>
2024-12-21 06:04:03 +0000

    login page created

--- lib/core/constant.dart
+++ lib/core/constant.dart
@@ -7,4 +7,12 @@ const String welcome = "Welcome";
 const String toastText = "Invalid Email or Password";
 const String noDataFoundInYourCollection = "No data found in your collection";
 
+// login
 
+const String loginHeader = "Get started!";
+const String loginHeader1 = "Join our amazing community of music lovers";
+const String google = "Continue with Google";
+const String facebook = "Continue with Facebook";
+const String email = "Continue with Email";
+const String artist = "Are you an artist?";
+const String goto = "Go to Musixmatch Pro";
--- lib/login/screen/login_page.dart
+++ lib/login/screen/login_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:musix_match_app/core/constant.dart';
+import 'package:musix_match_app/login/widgets/login_button.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,14 +13,74 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("Get started!",style: TextStyle(fontSize: 23),),
-            Text("Join our amazing community of music lovers")
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                  height: 80, child: Image.asset("assets/musixmatchlogo.jpeg")),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                loginHeader,
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              const Align(
+                  alignment: Alignment.center,
+                  child: Text(loginHeader1, style: TextStyle(fontSize: 18))),
+              const SizedBox(height: 30),
+              // const Spacer(),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                child: LoginButton(
+                  backgroundColor: Colors.grey.shade400,
+                  header: google,
+                  icon: const FaIcon(
+                    FontAwesomeIcons.google,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: LoginButton(
+                  backgroundColor: Colors.blue,
+                  header: facebook,
+                  icon: FaIcon(
+                    FontAwesomeIcons.facebook,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: LoginButton(
+                  backgroundColor: Colors.grey.shade400,
+                  header: email,
+                  icon: const FaIcon(
+                    FontAwesomeIcons.mailBulk,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              const Text(
+                artist,
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                goto,
+                style: TextStyle(fontSize: 18,),
+              ),
+            ],
+          ),
         ),
       ),
     );
